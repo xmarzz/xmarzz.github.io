5e62b4195a2f28b6522b30ac98a0ad9e7392cb38
Marz <sathickjaffer44@gmail.com>
2024-02-17 22:40:12 +0530

    updated mywork

--- src/myWork/myWork.js
+++ src/myWork/myWork.js
@@ -7,19 +7,35 @@ const myWork=()=>{
     const projects={
         "Face 2 Face":{
             title: "Face 2 Face",
-            desc: "",
-            link: "",
-            open: ""
+            desc: "A web application designed to be mobile-friendly, tailored specifically to a local salon.",
+            link: "https://github.com/xmarzz/Face2Face",
+            open:"https://face2-face.vercel.app/",
+            techstack: "Next.js, Tailwind css, Typescript"
         },
         "Artwork":{
-
+            title:"Artwork web page",
+            desc:"A platform where users can upload photos of their drawings or artworks on one page, and on another page, users can upload their music.",
+            techstack:"Next.js, Cloudinary(backend), Auth 2.0",
+            link:"https://github.com/xmarzz/artWork"
         },
         "Hiro Bot":{
-
+            title: "Hiro Telegram Bot",
+            desc:"",
+            link:"https://github.com/xmarzz/Hiro-bot",
+            techstack:"Javascript, Node.js, Telegram API"
+        },
+        "web Srcapper":{
+            title:"web scrapper",
+            desc:" The scraper is designed to navigate through a list of specified websites, extract relevant text and titles.",
+            link: "https://github.com/xmarzz/web-scrapper",
+            techstack:"Python, pip(package manager), virtualenv"
         },
         "Portfolio Page":{
-
-        }
+            title : "Portfolio Page",
+            desc:"A straightforward, responsive, and distinctive portfolio page",
+            link:"https://github.com/xmarzz/xmarzz.github.io",
+            techstack:"React.js, Javascript"
+        },
     }
 
     return(
