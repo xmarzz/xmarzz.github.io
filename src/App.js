8e1203c5f528057158fb4f374c33e2a3d092f558
Marz <sathickjaffer44@gmail.com>
2026-09-24 09:13:46 +0530

    fix 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css"
 import HomePage from "./HomePage/HomePage.js"
 import { BrowserRouter, Routes, Route} from "react-router-dom"; 
-import Blog from "./Blog/Blog"
 import BlogPost from "./Blog/BlogPost"
+import Blog from "./Blog/Blog"
 
 const App =()=> {
      return(
